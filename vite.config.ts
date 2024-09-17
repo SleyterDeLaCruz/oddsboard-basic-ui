398ffabd1921b37cb3c774e1f456d67be234c48d
Sleyter De La Cruz <171729975+SleyterDeLaCruz@users.noreply.github.com>
2024-09-17 11:13:25 -0400

    undo error

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/oddsboard-basic-ui',
+  base: '/oddsboard-basic-ui/',
   plugins: [react()],
   build: {
     outDir: 'dist',
