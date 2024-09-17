21e20e25e08a2398608ebb2ffebbef15af225651
Sleyter De La Cruz <171729975+SleyterDeLaCruz@users.noreply.github.com>
2024-09-17 16:09:08 -0400

    fixx route

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/oddsboard-basic-ui/', 
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/oddsboard-basic-ui/' : '/',  // Use '/' for local dev and '/oddsboard-basic-ui/' for production
   plugins: [react()],
   build: {
-    outDir: 'dist', 
+    outDir: 'dist',
   },
-});
+}));
