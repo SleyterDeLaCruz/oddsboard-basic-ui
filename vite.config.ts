d98d01bd941a19ae0f3a6777bb58939dcc56696e
Sleyter De La Cruz <171729975+SleyterDeLaCruz@users.noreply.github.com>
2024-09-17 18:19:55 -0400

    use correct asset

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/oddsboard-basic-ui/' : '/',  // Use '/' for local dev and '/oddsboard-basic-ui/' for production
+export default defineConfig({
+  base: '/oddsboard-basic-ui/', // Make sure this is correct
   plugins: [react()],
   build: {
     outDir: 'dist',
   },
-}));
+});
+
+
