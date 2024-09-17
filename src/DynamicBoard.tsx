bdb2b3e5574d85f4083bd4aa0f3cd0c9461f947f
Sleyter De La Cruz <171729975+SleyterDeLaCruz@users.noreply.github.com>
2024-09-17 17:26:12 -0400

    dist fix

--- src/DynamicBoard.tsx
+++ src/DynamicBoard.tsx
@@ -216,7 +216,7 @@ export function DynamicBoard() {
   }, []);
 
   return (
-  <main className="main" style={{ backgroundImage: `url(${import.meta.env.BASE_URL}assets/oddsboard.png)` }}>
+  <main className="main" style={{ backgroundImage: `url(/oddsboard-basic-ui/assets/oddsboard.png)` }}>
       <Header title={title} keyMarkets={keyMarkets} />
       <section className="column" ref={eventListRef} style={{ maxHeight: '100vh', overflow: 'auto' }}>
         {events.map((event) => (
